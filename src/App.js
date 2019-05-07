71e906215d524095a901d76d8ecbbbededdcc4a0
Jorge Daniel Aguilar Rivera <jdaguilar@fmf.mx>
2019-05-07 12:39:18 -0500

    change port

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, DevOps <code>devops.js</code> and change your mind.
         </p>
       </div>
     );
